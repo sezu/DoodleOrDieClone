2f825c418f69d352703ec4abee210a511b5224b0
TJ Hawbaker <Hawbaker.TJ@gmail.com>
2014-03-19 10:20:00 -0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoodleOrDieClone::Application.config.secret_key_base = '945d4fb9ffb62ad9654dbb09f0580b89ef7fbb81adbe6376fa2d1c4b857da893210f05951933551c96f1e56af11a3008462a3bf8cbc6bf4c5a8eede9413fa275'
